57090aef57bc9b90ff7efbd6f587a64032e659df
Arvid <kolsmyr.arvid@abbgymnasiet.se>
2022-08-26 10:56:56 +0200

    'commit'

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from textgenrnn import textgenrnn
 
 from os import listdir
 from os.path import isfile, join
-onlyfiles = [f for f in listdir('./') if isfile(join('./', f))]
-print(onlyfiles)
 
-bow = {}
+textgen = textgenrnn()
+
+textgen.load("./filename_trained_network.hdf5")
+
+onlyfiles = [f for f in listdir('./') if isfile(join('./', f))]
 
 for i in onlyfiles:
     textgen.train_from_file('./'+i, num_epochs=1)
-    textgen.generate()
-        
+    textgen.generate(5, temperature=1.0)
+    textgen.save('./filename_trained_network.hdf5')
 
-print(bow)
+textgen.generate(1000, temperature=1.0)
             
         
